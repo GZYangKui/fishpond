b4565e7d0b041274197528e865e3d925266d4292
yangkui <752544765@qq.com>
2022-03-05 23:15:56 +0800

    添加module-info.java文件

---
+++ protocol/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module cn.navclub.fishpond.protocol {
+    requires io.vertx.core;
+    exports cn.navclub.fishpond.protocol;
+    exports cn.navclub.fishpond.protocol.model;
+    exports cn.navclub.fishpond.protocol.impl;
+}
